ba422bdc1a5cd4cbb3382890be021c9dddbe0db4
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-01-20 23:53:16 +0100

    (samples/web_app_basic) map host names on to IP addresses

--- samples/web_app_basic/mgmt_bootstrap.sh
+++ samples/web_app_basic/mgmt_bootstrap.sh
@@ -6,3 +6,11 @@ apt-get update -q && apt-get install -y \
     software-properties-common \
     ansible
 
+# configure hosts file for Vagrant internal network
+cat >> /etc/hosts <<EOL
+
+# vagrant env nodes
+10.10.32.10 mgmt
+10.10.64.1  web1
+EOL
+

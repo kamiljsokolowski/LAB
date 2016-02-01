743ad9879b2d78c6440e729a0bf817e7eb77c522
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-01 22:50:44 +0100

    (samples/web_app_basic) append Ansible configuration to infrastructure provisioning

--- samples/web_app_basic/mgmt_bootstrap.sh
+++ samples/web_app_basic/mgmt_bootstrap.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
 
 # add official Ansible repo & install Ansible
-apt-add-repository -y ppa:ansible/ansible
-apt-get update -q && apt-get install -y \
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update -q && sudo apt-get install -y \
     software-properties-common \
     ansible
 
 # configure hosts file for Vagrant internal network
-cat >> /etc/hosts <<EOL
+#sudo cat >> /etc/hosts <<EOL
+sudo tee -a /etc/hosts > /dev/null <<EOL
 
 # vagrant env nodes
 10.10.32.10 mgmt
 10.10.32.21  web1
 EOL
+
+# configure Ansible
+cat >> ${HOME}/ansible.cfg <<EOL
+[defaults]
+hostfile=/home/vagrant/inventory.ini
+EOL
+cat >> ${HOME}/inventory.ini <<EOL
+[web]
+web1
+EOL
+
+### VPS config
+# generate SSH key and distribute it to all nodes
+#if [ ! -f ${HOME}/.ssh/id_rsa.pub ]; then
+#    ssh-keygen -t rsa -b 2048
+#fi
+
 

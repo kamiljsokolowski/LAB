e9a632b0a1912820b6c5670dde21a7bb341ac84a
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-01 23:58:44 +0100

    (samples/web_app_basic) provision a (basic) VPS

--- samples/web_app_basic/mgmt_bootstrap.sh
+++ samples/web_app_basic/mgmt_bootstrap.sh
@@ -27,8 +27,7 @@ EOL
 
 ### VPS config
 # generate SSH key and distribute it to all nodes
-#if [ ! -f ${HOME}/.ssh/id_rsa.pub ]; then
-#    ssh-keygen -t rsa -b 2048
-#fi
-
+if [ ! -f ${HOME}/.ssh/id_rsa.pub ]; then
+    ssh-keygen -t rsa -b 2048 -f ${HOME}/.ssh/id_rsa -q -N ""
+fi
 
